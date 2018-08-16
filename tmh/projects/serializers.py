6baa6831c6cf3922d7700974f28c168904747fd9
manticarodrigo <manticarodrigo@gmail.com>
2018-08-15 23:22:29 -0400

    added 'me' action to ListProject viewset

--- tmh/projects/serializers.py
+++ tmh/projects/serializers.py
@@ -5,19 +5,4 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = (
-            'id',
-            'created_date',
-            'modified_date',
-            'start_date',
-            'end_date',
-            'style',
-            'room_type',
-            'video_url',
-            'zipcode',
-            'status',
-            'designer_note',
-            'final_note',
-            'revision_count',
-            'user'
-        )
+        fields = '__all__'
--- tmh/projects/views.py
+++ tmh/projects/views.py
@@ -1,4 +1,8 @@
+from django.core import serializers
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Project
 from .serializers import ProjectSerializer
@@ -7,6 +11,12 @@ from .serializers import ProjectSerializer
 class ListProject(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    @action(methods=['get'], detail=False, permission_classes=[])
+    def me(self, request, pk=None):
+        user = request.user
+        projects = ProjectSerializer(Project.objects.filter(user__pk=user.id), many=True).data
+        return Response(projects)
 
 
 class DetailProject(viewsets.ModelViewSet):
