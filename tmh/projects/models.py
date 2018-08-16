674d964301e966e1ab8eae9611e9b8da2516ea45
manticarodrigo <manticarodrigo@gmail.com>
2018-08-15 22:09:48 -0400

    added project properties to model and serializer

--- tmh/projects/models.py
+++ tmh/projects/models.py
@@ -1,10 +1,24 @@
 from django.db import models
+from tmh.users.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(auto_now=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    style = models.CharField(max_length=200)
+    room_type = models.CharField(max_length=200)
+    video_url = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    designer_note = models.CharField(max_length=200)
+    final_note = models.CharField(max_length=200)
+    revision_count = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
     def __str__(self):
         """A string representation of the model."""
-        return self.title
+        return self.room_type
--- tmh/projects/serializers.py
+++ tmh/projects/serializers.py
@@ -4,9 +4,20 @@ from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Project
         fields = (
             'id',
-            'title',
-            'description',
+            'created_date',
+            'modified_date',
+            'start_date',
+            'end_date',
+            'style',
+            'room_type',
+            'video_url',
+            'zipcode',
+            'status',
+            'designer_note',
+            'final_note',
+            'revision_count',
+            'user'
         )
-        model = Project
