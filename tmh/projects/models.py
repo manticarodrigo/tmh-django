39d3e587ae6496e035baeaf92e95a65b043965b8
manticarodrigo <manticarodrigo@gmail.com>
2018-08-16 21:03:02 -0400

    added room and status tuples, returning user serializer in project serializer

--- tmh/projects/models.py
+++ tmh/projects/models.py
@@ -2,23 +2,48 @@ from django.db import models
 from tmh.users.models import User
 from django.utils import timezone
 
-# Create your models here.
 class Project(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     modified_date = models.DateTimeField(auto_now=True)
-    start_date = models.DateTimeField(blank=True, null=True)
-    end_date = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    ROOM_CHOICES = (
+        ('BEDROOM', 'Bedroom'),
+        ('LIVING_ROOM', 'Living Room'),
+        ('MULTIPURPOSE_ROOM', 'Open Layout'),
+        ('STUDIO', 'Studio'),
+        ('DINING_ROOM', 'Dining Room'),
+        ('HOME_OFFICE', 'Office'),
+    )
+
+    room = models.CharField(max_length=100, choices=ROOM_CHOICES)
+
+    STATUS_CHOICES = (
+        ('DETAILS', 'Details'),
+        ('DESIGN', 'Design'),
+        ('CONCEPTS', 'Concepts'),
+        ('FLOOR_PLAN', 'Floor Plan'),
+        ('REQUEST_ALTERNATIVES', 'Request Alternatives'),
+        ('ALTERNATIVES_READY', 'Alternatives Ready'),
+        ('FINAL_DELIVERY', 'Final Delivery'),
+        ('SHOPPING_CART', 'Shopping Cart'),
+        ('ESTIMATE_SHIPPING_AND_TAX', 'Estimate Shipping & Tax'),
+        ('CHECKOUT', 'Checkout'),
+        ('ARCHIVED', 'Archived'),
+    )
+    
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+
     style = models.CharField(max_length=200)
-    room_type = models.CharField(max_length=200)
     video_url = models.CharField(max_length=200)
     zipcode = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
     designer_note = models.CharField(max_length=200)
     final_note = models.CharField(max_length=200)
     revision_count = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        """A string representation of the model."""
-        return self.room_type
+        '''A string representation of the model.'''
+        return self.room
--- tmh/projects/serializers.py
+++ tmh/projects/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, User
+from tmh.users.serializers import UserSerializer
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Project
         fields = '__all__'
+
+    created_date = serializers.DateTimeField(read_only=True)
+    room = serializers.ChoiceField(choices=Project.ROOM_CHOICES)
+    status = serializers.ChoiceField(choices=Project.STATUS_CHOICES)
+    user = UserSerializer()
